8054ac98de77ef7322d2f2601f942f55288e3c3f
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-26 00:23:23 +0500

    INFRA: WordNest Project Initialize

---
+++ WorldNest/Models/Countries/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace WorldNest.Models.Countries
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public double Area { get; set; }
+        public string Language { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ WorldNest/Models/Oceans/Ocean.cs
@@ -0,0 +1,15 @@
+﻿namespace WorldNest.Models.Oceans
+{
+    public class Ocean
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public string Location { get; set; }
+        public double Depth { get; set; }
+        public double Salinity { get; set; }
+        public double Temperature { get; set; }
+        public string Biodiversity { get; set; }
+        public string MajorCurrents { get; set; }
+    }
+}
