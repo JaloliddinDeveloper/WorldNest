ff7649a6c4fa0aa08f72f007bf3adb730571317b
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-01-19 08:22:18 +0500

    24

--- WorldNest/Program.cs
+++ WorldNest/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using WorldNest.Services.Countries;
 using WorldNest.Services.Oceans;
 
@@ -7,30 +8,40 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+      
+
+        // Servislarni qo'shish
         builder.Services.AddControllersWithViews();
         builder.Services.AddScoped<CountryService>();
         builder.Services.AddScoped<OceanService>();
 
         var app = builder.Build();
 
+        // Iftimoiy muhiti tekshirish
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
         }
 
+        // HTTP so'rovlarining HTTPS-ga yo'naltirilishini ta'minlash
         app.UseHttpsRedirection();
 
+        // Statik fayllarni ko'rsatish
         app.UseStaticFiles();
 
+        // Routing tizimini ishga tushurish
         app.UseRouting();
 
+        // Avtorizatsiya tizimini ishga tushurish
         app.UseAuthorization();
 
+        // Kontroller yo'riqnomalarini belgilash
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
+        // Ilovani ishga tushurish
         app.Run();
     }
 }
